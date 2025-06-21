a46b7bbf8fb2973857c3e2ff930c5771484a33e9
Cristian-fcv <cristian.sanchez.fcv@gmail.com>
2025-06-20 22:52:13 -0600

    primer cambio

---
+++ src/numeros.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+
+public class numeros {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int[] numeros = {250, 100, 23, 145, 122, 432, 107, 109, 265, 194, 1};
+
+        // Ordenamos el arreglo de menor a mayor
+        Arrays.sort(numeros);
+
+        // Mostramos los números ordenados
+        System.out.println("Números ordenados de menor a mayor:");
+        for (int numero : numeros) {
+            System.out.print(numero + " ");
+        }
+
+	}
+
+}
