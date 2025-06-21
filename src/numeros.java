e209185be86ec7c5f13084bfc7264891b82a5925
Cristian-fcv <cristian.sanchez.fcv@gmail.com>
2025-06-20 22:58:31 -0600

    agergando nuevos cambios

--- src/numeros.java
+++ src/numeros.java
@@ -5,6 +5,8 @@ public class numeros {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		
+		
 		int[] numeros = {250, 100, 23, 145, 122, 432, 107, 109, 265, 194, 1};
 
         // Ordenamos el arreglo de menor a mayor
@@ -15,6 +17,8 @@ public class numeros {
         for (int numero : numeros) {
             System.out.print(numero + " ");
         }
+        
+        System.out.println("prueba de github");
 
 	}
 
